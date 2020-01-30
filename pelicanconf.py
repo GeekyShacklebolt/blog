5e3596d07d4c3d3c2aac72fe419c5e030fcf6a5e
GeekyShacklebolt <shivasaxena911@gmail.com>
2020-01-30 15:36:03 +0530

    fix(pelicanconf): Add my custom configurations
    * Add custom theme path
    * Add social icons
    * Add custom theme variables

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,29 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+#############################
+#    My Custom Additions    #
+#############################
+THEME = '/home/shiva/.virtualenvs/pelicanblog/lib/python3.8/site-packages/pelican/themes/pelican-clean-blog'
+RELATIVE_URLS = True
+
+# Social icons
+SOCIAL = (
+    ('github', 'https://github.com/geekyshacklebolt'),
+    ('linkedin', 'https://linkedin.com/in/shiva-saxena'),
+    ('instagram', 'https://instagram.com/geekyshacklebolt'),
+    ('twitter', 'https://twitter.com/geekyshacklebolt'),
+    ('wordpress', 'https://geekyshacklebolt.wordpress.com'),
+    ('pencil', 'http://www.thegeekyway.com/author/geekyshacklebolt'),
+    ('envelope', 'mailto:shivasaxena911@gmail.com'),
+)
+
+SITESUBTITLE = 'Blog'
+SHOW_SITESUBTITLE_IN_HTML = 'True'
+HEADER_PAGE_TITLE = 'A Personal Blog'
+SHOW_CREDIT_BY_AUTHOR = 'True'
+NAVBAR_BRAND = 'GeekyShacklebolt'
+HEADER_COVER = 'images/my-blog-header-bg.jpg'
+AUTHOR_NICK = 'GeekyShacklebolt'
+
